40a547ddf16825afc0938e11ec433921bce9ca0b
kambam-sathwik <sathwikkambam@gmail.com>
2024-04-27 06:58:59 +0530

    created project

---
+++ index.js
@@ -0,0 +1,10 @@
+// Creating Elements & DOM Relations
+// Deliverables:
+
+// 1. Inside the first Div, after the main heading add a sub-heading (h3 tag) "Buy high quality organic fruits online".
+
+// 2. Make the sub-heading text italic.
+
+// 2. Inside the second Div, before the unordered list add a paragraph tag showing "Total fruits: 4".
+
+// 3. On this paragraph tag set an id of "fruits-total".
